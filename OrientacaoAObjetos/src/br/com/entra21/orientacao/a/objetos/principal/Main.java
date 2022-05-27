e4d98619c57b4c87850a64162a9c0d28e161a35c
ariana-ssilva <arianamoreira5021@gmail.com>
2022-05-26 22:05:24 -0300

    Aula sobre conceitos de POO e polimorfismo com interfaces.

--- OrientacaoAObjetos/src/br/com/entra21/orientacao/a/objetos/principal/Main.java
+++ OrientacaoAObjetos/src/br/com/entra21/orientacao/a/objetos/principal/Main.java
@@ -6,9 +6,19 @@ import br.com.entra21.orientacao.a.objetos.principal.aula01.classes.Aluno;
 import br.com.entra21.orientacao.a.objetos.principal.aula01.classes.Professor;
 import br.com.entra21.orientacao.a.objetos.principal.aula02.heranca.Diretor;
 import br.com.entra21.orientacao.a.objetos.principal.aula02.heranca.Funcionario;
+import br.com.entra21.orientacao.a.objetos.principal.aula02.heranca.Pessoa;
 import br.com.entra21.orientacao.a.objetos.principal.aula03.polimorfismo.Atleta;
 import br.com.entra21.orientacao.a.objetos.principal.aula03.polimorfismo.Nadador;
 import br.com.entra21.orientacao.a.objetos.principal.aula03.polimorfismo.Velocista;
+import br.com.entra21.orientacao.a.objetos.principal.aula04.conceitos.poo.Ponto;
+import br.com.entra21.orientacao.a.objetos.principal.aula04.conceitos.poo.Reta;
+import br.com.entra21.orientacao.a.objetos.principal.aula04.interfaces.Aviao;
+import br.com.entra21.orientacao.a.objetos.principal.aula04.interfaces.Cachorro;
+import br.com.entra21.orientacao.a.objetos.principal.aula04.interfaces.Capivara;
+import br.com.entra21.orientacao.a.objetos.principal.aula04.interfaces.Gato;
+import br.com.entra21.orientacao.a.objetos.principal.aula04.interfaces.Hero;
+import br.com.entra21.orientacao.a.objetos.principal.aula04.interfaces.Humano;
+import br.com.entra21.orientacao.a.objetos.principal.aula04.interfaces.Planta;
 
 
 public class Main {
@@ -29,6 +39,8 @@ public class Main {
 			System.out.println("1 - Classes e objetos");
 			System.out.println("2 - Herança");
 			System.out.println("3 - Polimorfismo");
+			System.out.println("4 - Aprender conceitos POO");
+			System.out.println("5 - Aprender polimorfismo com interface");
 			opcao = entrada.nextByte();
 
 			switch (opcao) {
@@ -46,8 +58,17 @@ public class Main {
 			case 3:
 				aprenderPolimorifsmo();
 				break;
+				
+			case 4:
+				aprenderConceitosPOO();
+				break;
+				
+			case 5:
+				aprenderPolimorfismoInterface();
+				break;	
 
 			default:
+				System.out.println("C'monnnnnnnn :(");
 				break;
 			}
 
@@ -55,6 +76,7 @@ public class Main {
 
 	}
 
+	
 	private static void aprenderClasses() {
 		// instanciando um objeto da classe Professor na variavel professorJava
 		Professor professorJava = new Professor();
@@ -151,5 +173,55 @@ public class Main {
 		flash.aprenderComDerrota();
 	}
 	
+	private static void aprenderConceitosPOO() {
+		// A classe Ponto tem alta coesao pois ele e preciso nas suas responsabilidades
+		Ponto pontoAlto = new Ponto(30, 10000);
+		Ponto esquerdaBaixo = new Ponto(-20, -100);
+		
+		// A classe Reta e agregada por pontos que podem nao estar incializados
+		// A associacao de reta e ponto e de 1 para muitos, respectivamente 
+		Reta torta = new Reta();
+		Reta diagonal = new Reta(pontoAlto, esquerdaBaixo);
+	}
+	
+	private static void aprenderPolimorfismoInterface() {
+		Humano ariana = new Humano();
+		ariana.setName("Ariana");
+		
+		ariana.apresentarSe();
+		ariana.alimentar("Macarroni");
+		ariana.locomover();
+		ariana.comunicar("Hola que tal ?");
+		
+		Gato vezu = new Gato("Abobora", new Pessoa("Ariana", (byte) 22, ""));
+		vezu.alimentar("Racao");
+		vezu.comunicar("Quero papar");
+		vezu.locomover();
+		
+		Capivara robson = new Capivara();
+		robson.alimentar("agua");
+		robson.comunicar("");
+		robson.locomover();
+		
+		Planta maracuja = new Planta(" Passiflora edulis Sims");
+		maracuja.alimentar("chocolatin");
+		maracuja.comunicar("Tá sol ai?");
+		maracuja.locomover();
+		
+		Cachorro thor = new Cachorro("Thor", new Pessoa("Ariana", (byte) 22, ""));
+		thor.alimentar("o que vier");
+		thor.comunicar("joga a bolinha humano otario");
+		thor.locomover();
+		
+		Hero superHeroiSensacional = new Hero("todos", "Super Heroi Heroico");
+		superHeroiSensacional.alimentar("exatamente tudo");
+		superHeroiSensacional.comunicar("Eu sou a força");
+		superHeroiSensacional.locomover();
+		
+		Aviao comercial = new Aviao();
+		comercial.setModelo("Boeing 737");
+		System.out.println(comercial.frear());
+		comercial.acelerar();
+	}
 	
 }
